b8e994416b75063d4de1f72d83765691e6e0208c
594-666 <polar940301@gmail.com>
2024-06-27 16:02:46 +0800

    gg

---
+++ src/handleEvent/messageCreate/features/ii45i4.ts
@@ -0,0 +1,16 @@
+import { Message } from "discord.js";
+import { MessageFeature } from "../../../classes/MessageFeature";
+import { byChance, randomInt } from "../../../functions/general/number";
+import homo from "../../../functions/general/homo";
+
+export = new class ii45i4 extends MessageFeature {
+    filter(message: Message<boolean>, ...params: any): boolean {
+        return /\d/.test(message.content) && byChance(5);
+    }
+    async exe(message: Message<boolean>, ...params: any): Promise<void> {
+        const numbers = message.content.match(/\d+/gm) as RegExpMatchArray;
+        const theOneChosenShit = numbers[randomInt(0, numbers.length - 1)];
+        const result = homo(Number(theOneChosenShit));
+        message.reply(`${theOneChosenShit} = \`${result}\``);
+    }
+}
