1675e2052590996d5d0a06f870dc6da660903e1d
594-666 <polar940301@gmail.com>
2024-06-29 01:16:39 +0800

    log

--- src/functions/general/log.ts
+++ src/functions/general/log.ts
@@ -1,12 +1,27 @@
 import chalk from "chalk";
-import { CommandInteraction } from "discord.js";
+import { CommandInteraction, Message } from "discord.js";
+import { prefix } from "../../app";
 
 const logCommand = (interaction: CommandInteraction): void => {
     console.log(`${chalk.bgBlackBright(new Date().toISOString())}\n` +
-        `\t${chalk.blueBright(interaction.user.username)} at ${chalk.greenBright(interaction.guild?.name)}\n` +
+        `\t${chalk.blueBright(interaction.user.username)} in ${chalk.greenBright(interaction.guild?.name)}\n` +
         `\t\t/${chalk.yellow(interaction.commandName)}`);
 }
 
+const logMsgCommand = (message: Message, command: string): void => {
+    console.log(`${chalk.bgBlackBright(new Date().toISOString())}\n` +
+        `\t${chalk.blueBright(message.author.username)} in ${chalk.greenBright(message.guild?.name)}\n` +
+        `\t\t${prefix}${chalk.yellow(command)}`);
+}
+
+const logMsgFeature = (message: Message, feature: string): void => {
+    console.log(`${chalk.bgBlackBright(new Date().toISOString())}\n` +
+        `\t${chalk.blueBright(message.author.username)} in ${chalk.greenBright(message.guild?.name)}\n` +
+        `\t\ttriggerred${chalk.yellow(feature)}`);
+}
+
 export {
-    logCommand
+    logCommand,
+    logMsgCommand,
+    logMsgFeature
 }
--- src/handleEvent/messageCreate.ts
+++ src/handleEvent/messageCreate.ts
@@ -1,44 +1,10 @@
-import { byChance, randomInt, restrictRange } from '../functions/general/number';
-import { AttachmentBuilder, Message } from 'discord.js';
+import { Message } from 'discord.js';
 import { shouldRpMsg } from '../functions/config/shouldReply';
-import { getCmdInfo } from '../functions/discord/msgCommand';
-import { delaySec, doAfterSec } from '../functions/general/delay';
-import { prefix } from '../app';
-import path from 'node:path';
-import fs from 'node:fs';
-import { earn500 } from '../functions/discord/cmps';
-import homo from '../functions/general/homo';
 import Selecter from './messageCreate/Selecter';
 
 const create = async (message: Message): Promise<void> => {
-
     if (shouldRpMsg(message))
         Selecter.exe(message);
-    /*{
-    // else if (/\d/.test(message.content) && byChance(5)) {
-    //     const numbers = message.content.match(/\d+/gm) as RegExpMatchArray;
-    //     const theOneChosenShit = numbers[randomInt(0, numbers.length - 1)];
-    //     const result = homo(Number(theOneChosenShit));
-    //     message.reply(`${theOneChosenShit} = \`${result}\``);
-    } else if (byChance(1)) {
-    //     do {
-    //         try { message.channel.sendTyping(); } catch (e) { }
-    //         delaySec(randomInt(8, 15));
-    //     } while (byChance(10))
-    // } else if (message.content.includes('114514')) {
-    //     message.channel.send({
-    //         files: [new AttachmentBuilder(fs.readFileSync(path.join(__dirname, '..', '..', '..', 'media', 'pic', '114514.webp')))]
-    //     });
-    } else if (message.content.includes('臭')) {
-        if (byChance(50)) message.reply('好臭');
-    } else if (byChance(2) && !message.channel.isDMBased()) {
-        message.channel.send({
-            content: 'Being poor?',
-            components: earn500(false)
-        });
-    }
-}
-*/
-}
+};
 
 export = create;
