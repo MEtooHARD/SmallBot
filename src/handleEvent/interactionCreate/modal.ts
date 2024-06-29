069d970ba0432e92760357f8ec831c3d88f7e88d
594-666 <polar940301@gmail.com>
2024-06-29 13:30:29 +0800

    fix order list

--- src/handleEvent/interactionCreate/modal.ts
+++ src/handleEvent/interactionCreate/modal.ts
@@ -1,19 +1,25 @@
 import { ModalSubmitInteraction } from 'discord.js';
 import { getSvcDir, getSvcInfo } from '../../functions/discord/service';
 import path from 'node:path';
+import fs from 'node:fs';
 import chalk from 'chalk';
+import rootPath from 'get-root-path';
 
 const modal = (interaction: ModalSubmitInteraction) => {
-    const svcInfo = getSvcInfo(interaction.customId);
+    if (!interaction.customId.startsWith('$')) {
+        const svcInfo = getSvcInfo(interaction.customId);
 
-    if (!interaction.customId.startsWith('$'))
-        if (svcInfo?.length) {
-            const dir = getSvcDir(path.join(__dirname, '..', '..', '..', 'features'), svcInfo[0]);
+        if (svcInfo.length) {
+            const dir = getSvcDir(path.join(rootPath, 'dist', 'features'), svcInfo[0]);
+            // const action = fs.readdirSync(path.join(rootPath, 'src', ''));
             if (dir.length) require(dir)(interaction, svcInfo);
+            console.log(dir);
+            console.log(interaction.customId);
             console.log(chalk.yellow('call ') + svcInfo[0]);
         } else {
 
         }
+    }
 }
 
 export = modal;
